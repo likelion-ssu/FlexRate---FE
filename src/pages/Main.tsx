dee8fa6d18b6ccafcd7da14be17a85126837b40c
허동윤 <dongyun@heodong-yun-ui-MacBookPro.local>
2023-10-28 14:29:46 +0900

    feat:add loan apply page and add login page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,13 @@
-
+import AreaChart from '@/components/AreaChart';
 import SemiCircleCharts from '@/components/SemiCircleChart';
 
 const Main = () => {
-  const tmp = 67;
+  const tmp = 37;
   return (
     <div>
       <div>main page</div>
       <SemiCircleCharts num={tmp} />
-
+      <AreaChart />
     </div>
   );
 };
