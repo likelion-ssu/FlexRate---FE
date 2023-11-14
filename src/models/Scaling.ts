3a81eccf3a5dc796dd4de7f56a2eae3978bef58f
신수연 <sooy@shinsooyeon-MacBookPro.local>
2023-11-14 19:14:57 +0900

    feat: Scaling 모델 구현

---
+++ src/models/Scaling.ts
@@ -0,0 +1,31 @@
+export interface Features {
+  credit_score: number;
+  yearly_income: number;
+  company_enter_month: number;
+  existing_loan_cnt: number;
+  existing_loan_amt: number;
+  debt_rate: number;
+  income_type_EARNEDINCOME2: number;
+  income_type_FREELANCER: number;
+  income_type_OTHERINCOME: number;
+  income_type_PRACTITIONER: number;
+  income_type_PRIVATEBUSINESS: number;
+  employment_type_기타: number;
+  employment_type_일용직: number;
+  employment_type_정규직: number;
+  houseown_type_배우자: number;
+  houseown_type_자가: number;
+  houseown_type_전월세: number;
+}
+
+export default function Scaling(features: Features): Features {
+  features.credit_score = (features.credit_score - 60) / (1000 - 60);
+  features.yearly_income = features.yearly_income / 10000000000;
+  features.company_enter_month =
+    features.company_enter_month / 122.15616438356165;
+  features.existing_loan_cnt = features.existing_loan_cnt / 229;
+  features.existing_loan_amt = features.existing_loan_amt / 7512000000;
+  features.debt_rate = features.debt_rate / 10000000000.0;
+  // 다른 범주형 변수들은 그대로 유지
+  return features;
+}
