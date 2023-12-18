5b91bb3b6370b1e6936b8d351560db38b41fcda7
SooY2 <ssy67366736@gmail.com>
2023-12-18 18:53:14 +0900

    mypage 만드는중

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as S from '../styles/MypageStyle';
+import axiosInstance from '@/apis/axiosinstance';
 
 const Mypage = () => {
-  return <div>Mypage</div>;
+  let memberId: string | null;
+
+  useEffect(() => {
+    memberId = localStorage.getItem('memberid');
+    getLoanInfo();
+  }, []);
+  //정보 api 받아오기
+
+  //신용정보 api 받아오기
+  const getLoanInfo = () => {
+    //서버통신
+    axiosInstance
+      .get(`/mypage/${memberId}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  return (
+    <S.Wrapper>
+      <S.계정정보>
+        <h3>계정 정보</h3>
+        <S.ColumnBox>
+          <S.Column>
+            <p>이름</p>
+            <p>숭멋사</p>
+          </S.Column>
+        </S.ColumnBox>
+      </S.계정정보>
+      <S.신용평가정보></S.신용평가정보>
+    </S.Wrapper>
+  );
 };
 
 export default Mypage;
---
+++ src/styles/MypageStyle.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 5rem;
+`;
+
+export const 계정정보 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+`;
+export const ColumnBox = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1.7rem;
+  list-style: none;
+  padding: 0;
+`;
+export const Column = styled.li``;
+export const 신용평가정보 = styled.div``;
